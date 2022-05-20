1899434fad8cc9e1eceb719a57a50bae50b8a0f5
Craig Pearson <thepearson@gmail.com>
2022-05-21 06:41:54 +1200

    Code tidy

--- src/game.ts
+++ src/game.ts
@@ -4,30 +4,80 @@ import World from "./obj/world";
 import * as Vec2 from "vector2d";
 import Zomball from "./obj/zomball";
 
+/**
+ * The top-level Game class, manages setup, base loop, and events
+ */
 export default class Game {
   
+  /**
+   * Event for the canvs.onMouseMove event
+   */
   mouse_move: any = null;
+
+  /**
+   * Event for the canvas.onMouseDown event
+   */
   mouse_down: any = null;
+
+  /**
+   * Game canvas
+   */
   canvas: HTMLCanvasElement | null = null;
+
+  /**
+   * Canvas redering context
+   */
   context: CanvasRenderingContext2D;
-  //secondsPassed: number = 0;
-  //dt: number = 0;
+
+  /**
+   * Timestamp when last frame was processed
+   * used for calculating the deltaTime and
+   * FPS.
+   */
   oldTimestamp: DOMHighResTimeStamp = 0.0;
+
+  /**
+   * Game world object
+   */
   world: World | undefined;
+
+  /**
+   * Holder for FPS, useful when needing to render this within the game environment
+   */
   fps: number = 0;
 
+  /**
+   * Timer helper for adding enemy entities
+   */
   last_run_add: DOMHighResTimeStamp = 0;
 
+  /**
+   * [constructor description]
+   *
+   * @param   {HTMLCanvasElement}         canvas   This is the target canvas for our game
+   * @param   {CanvasRenderingContext2D}  context  This is the contet for the canvas
+   */
   constructor(canvas: HTMLCanvasElement, context: CanvasRenderingContext2D) {
     this.canvas = canvas;
     this.context = context;
 
+    // Set canvas dimensions
     this.canvas.width = Constants.GAME_SIZE.width;
     this.canvas.height = Constants.GAME_SIZE.height;
   }
 
-  setUp() {
+  /**
+   * Bootstrap function, used for setting  
+   * everything up prior to kicking off the renderLoop
+   *
+   * @return  {void}
+   */
+  public setUp(): void {
+
+    // Create world
     this.world = new World();
+
+    // Add player to game
     this.world.addPlayer(new Player(this.world));
 
     // Add events
@@ -44,41 +94,62 @@ export default class Game {
     }
   }
 
-  addZomball() {
+  /**
+   * Add's an enemy to the world
+   *
+   * @return  {void}
+   */
+  private addZomball(): void {
     const zom = new Zomball();
     zom.setSpawn();
     this.world?.addEntity(zom);
   }
 
-  processTimers(timestamp: DOMHighResTimeStamp) {
+  /**
+   * Processes any periodic timers
+   *
+   * @param   {DOMHighResTimeStamp}  timestamp  EPOC Milliseconds
+   *
+   * @return  {void}
+   */
+  private processTimers(timestamp: DOMHighResTimeStamp): void {
+
+    // Add enemy timer logic
     const add_zom_time = Constants.ZOMBALL_SPAWN_NEW_ZOMBALL_DELAY;
-    // Add zomball
     if (timestamp - this.last_run_add > add_zom_time) {
       this.addZomball();
       this.last_run_add = timestamp;
     }
-
-    // Make harder
-
   }
 
+  /**
+   * Process any game calculations, cascades down to the 
+   * World object and passes off any entity based calcs to there.
+   * Then trigger of all rendering for the world.
+   *
+   * @param   {DOMHighResTimeStamp}  timestamp  EPOC milliseconds
+   *
+   * @return  {void}
+   */
   process(timestamp: DOMHighResTimeStamp): void {
-    
+
+    // Short circut, these MUST exist
+    if (this.canvas == null || this.context == null) return;
+
     // Process any timers
     this.processTimers(timestamp);
 
     // Calculate the number of seconds passed since the last frame
     const dt : number = timestamp - this.oldTimestamp;
 
-    //this.secondsPassed = dt / 1000;
+    // Update timestamp for next process frame
     this.oldTimestamp = timestamp;
 
+    // Trigger World calcs
+    // TODO: async? Performance improovment at a later date.
     this.world?.process(dt, timestamp);
 
-    if (this.canvas == null) return;
-    if (this.context == null) return;
-
-    // Calculate fps
+    // Render things
     this.world?.render(this.canvas, this.context);
   }
 }
