fd323e684f2486e1c9da57c2e3a2bbc5111f296c
Craig Pearson <thepearson@gmail.com>
2022-05-22 08:05:44 +1200

    Chore, more documentation

--- src/obj/player.ts
+++ src/obj/player.ts
@@ -1,5 +1,5 @@
 import Entity from "./entity";
-import * as Vec2D from 'vector2d'
+import * as Vec2 from 'vector2d'
 import { Constants } from "../constants";
 import Color from "../util/color"
 import Pistol from "./pistol";
@@ -7,42 +7,79 @@ import Weapon from "./weapon";
 import World from "./world";
 
 
+/**
+ * Player entity
+ */
 export default class Player extends Entity {
 
+  /**
+   * Player primary weapon
+   */
   weapon_primary: Weapon | null = null;
+
+  /**
+   * Player secondary weapon
+   */
   weapon_secondary: Weapon | null = null;
 
+  /**
+   * @param   {World}  world  [world description]
+   */
   constructor(world: World) {
     super("player");
-
     this.world = world;
 
-    this.location = new Vec2D.Vector(
+    // Player location, centre of the world
+    this.location = new Vec2.Vector(
       Constants.GAME_SIZE.width / 2, 
       Constants.GAME_SIZE.height / 2
     );
 
+    // Set some attributes for the player
     this.size = Constants.PLAYER_SIZE;
     this.health = Constants.PLAYER_DEFAULT_HEALTH;
     this.color = new Color(128, 128, 255);
 
+    // Set starting weapons
     this.weapon_primary = new Pistol();
   }
 
-  takeDamage(value: number) {
+
+  /**
+   * Funciton for player to tak damage.
+   *
+   * @param   {number}  value  How much damage (healthpoints) should the player take
+   *
+   * @return  {void}
+   */
+  takeDamage(value: number): void {
     this.health -= value;
   }
 
-  firePrimaryWeapon(target: Vec2D.Vector) {
+
+  /**
+   * Fire player primary weapon
+   *
+   * @var {Vec2.Vector}
+   * 
+   * @return  {void}
+   */
+  firePrimaryWeapon(target: Vec2.Vector): void {
     this.weapon_primary?.fire(target, this.world!);
   }
 
-  fireSecondaryWeapon() {
+
+  /**
+   * Fire player secondary weapon: TODO
+   * 
+   *  @return  {void}
+   */
+  fireSecondaryWeapon(target: Vec2.Vector): void {
 
   }
 
   /**
-   * Process the zomballs movements and actions
+   * Process any player calcs
    */
   process(gameTime: number) {
     super.process(gameTime);
--- src/obj/world.ts
+++ src/obj/world.ts
@@ -7,83 +7,214 @@ import Color from "../util/color";
 import drawCircle from "../util/circle";
 import { Constants } from "../constants";
 
+
+/**
+ * Main world class, constructs everytrhing about the game world
+ */
 export default class World {
+
+  /**
+   * All game entities, these will all get 
+   * processed and rendered every tick
+   *
+   * @var  {Map<number, Entity>}
+   */
   entities: Map<number, Entity> = new Map();
 
+
+  /**
+   * Canvas element which is used for pre-rendering, 
+   * more static things
+   * 
+   * @var {HTMLCanvasElement}
+   */
   zomball_prerender: HTMLCanvasElement | null = null;
+
+  /**
+   * Canvas element used for prerendering dead zomballs
+   * 
+   * @var {HTMLCanvasElement}
+   */
   zomball_dead_prerender: HTMLCanvasElement | null = null;
+
+  /**
+   * Canvas element used for rebndering the blood
+   * 
+   * @var {HTMLCanvasElement}
+   */
   background: HTMLCanvasElement | null = null;
 
+  /**
+   * Player entity, handled slightly different from the 
+   * other entities in the game.
+   * 
+   * @var {Player}
+   */
   player: Player | null = null;
 
+  /**
+   * Player score
+   * 
+   * @var {number}
+   */
   player_score: number = 0;
+
+  /**
+   * Entity incremental number, used to track unique numerical
+   * id of all entities loaded into the world
+   * 
+   * @var {number}
+   */
   entity_id: number = 0;
 
+  /**
+   * Should we draw the targets?
+   * 
+   * @var {boolean}
+   */
   draw_target: boolean = false;
+
+  /**
+   * Target current location
+   *
+   * @var {Vec2.Vector}
+   */
   target: Vec2.Vector | null = null;
 
+  /**
+   * Helper to calculate FPS and framerates
+   * 
+   * @var {number}
+   */
   timeToDraw: number = 0;
+
+  /**
+   * Holder for deltatime
+   * 
+   * @var {number}
+   */
   dt: number = 0;
+
+  /**
+   * Previous frame timestamp
+   * 
+   * @var {DOMHighResTimeStamp}
+   */
   oldTimestamp: DOMHighResTimeStamp = 0.0;
+
+  /**
+   * Frames per second holder
+   * 
+   * @var {number}
+   */
   fps: number = 0;
 
+  /**
+   * Show debug?
+   * 
+   * @var {boolean}
+   */
   show_debug: boolean = true;
 
-  //Bullet bullet;
+  /**
+   * If we're drawing the targets, what is the base colour.
+   * 
+   * @var {Color}
+   */
   target_color: Color = new Color(255, 128, 128);
 
-  constructor() {
-    //this.entities = new Map();
-    //this.target_color = new Color(255, 128, 128);
-  }
 
-  getEntity(id: number): Entity | null {
+  /**
+   * Returns a game Entity
+   *
+   * @param   {number}  id  Entiti numerical ID
+   *
+   * @return  {Entity}
+   */
+  public getEntity(id: number): Entity | null {
     const entity = this.entities.get(id);
     if (entity) return entity;
     return null;
   }
 
-  addEntity(entity: Entity): void {
+  /**
+   * Adds an entity to the game
+   *
+   * @param   {Entity}  entity  Entity to add to the gameworld
+   *
+   * @return  {void}
+   */
+  public addEntity(entity: Entity): void {
     entity.id = this.entity_id;
     entity.world = this;
     this.entities.set(this.entity_id, entity);
     this.entity_id += 1;
   }
 
-  addPlayer(player: Player): void {
+  /**
+   * Add's the player to the game
+   *
+   * @param   {Player}  player  Player to add.
+   *
+   * @return  {void}
+   */
+  public addPlayer(player: Player): void {
     player.id = 999999;
     player.world = this;
     this.player = player;
   }
 
-  unsetTarget(): void {
+
+  /**
+   * Unset's the current target
+   *
+   * @return  {void}
+   */
+  public unsetTarget(): void {
     this.target = null;
   }
 
-  setTargetPosition(mouse_pos: Vec2.Vector) : void {
+
+  /**
+   * Sets the current target position
+   *
+   * @var {Vec2.Vector} mouse_pos   Target position
+   * 
+   * @return {void}
+   */
+  public setTargetPosition(mouse_pos: Vec2.Vector) : void {
     if (Constants.GAME_FLIP_SIGHTS == true) {
       if (this.player && this.player.location instanceof Vec2.Vector) {
         const vecCopy: Vec2.Vector = new Vec2.Vector(mouse_pos.x, mouse_pos.y);
         this.target = vecCopy.subtract(this.player.location)
       }
-      //this.target = (((mouse_pos-this.player.location)*1.5)*-1.0)+this.player.location;
     } else {
       this.target = mouse_pos;
     }
   }
 
+  /**
+   * Fire's a bullet
+   * 
+   * TODO: Rename this to support multi-weapons
+   *
+   * @return  {void}
+   */
   fireBullet(): void {
     if (this.player && this.player.location instanceof Vec2.Vector && this.target instanceof Vec2.Vector) {
       const dst = new Vec2.Vector(this.target.x, this.target.y);
       this.player.firePrimaryWeapon(dst);
-      // const bullet: Bullet = new Bullet(this.player?.location, this.target);
-      // bullet.world = this;
-      // this.addEntity(bullet);
-      // this.target = null;
     }
   }
 
-  drawTarget(context: CanvasRenderingContext2D) {
+  /**
+   * Helper function to draw the targets
+   *
+   * @param   {CanvasRenderingContext2D}  context  Canvas drawing context
+   *
+   * @return  {void}
+   */
+  public drawTarget(context: CanvasRenderingContext2D): void {
     if (this.target != null) {
       if (this.player instanceof Player)  {
         context.beginPath();
@@ -96,7 +227,16 @@ export default class World {
     }
   }
 
-  countEntities(type?: string): number {
+
+  /**
+   * Return count of entites by type, if no type specified, 
+   * then retunr count of all.
+   *
+   * @param   {string}  type  Entity Type|Name [optional]
+   *
+   * @return  {number}        Number of entities
+   */
+  public countEntities(type?: string): number {
     let count = 0;
 
     if (!type) return this.entities.size;
@@ -109,11 +249,27 @@ export default class World {
     return count;
   }
 
-  removeEntity(id: number) {
+  /**
+   * Remove an entity fromt he game
+   *
+   * @param   {number}  id  Numebrical ID of the entity 
+   *
+   * @return  {void}
+   */
+  public removeEntity(id: number): void {
     this.entities.delete(id);
   }
 
-  spatter(context: CanvasRenderingContext2D, location: Vec2.Vector): void {
+
+  /**
+   * Draws blood splatter
+   *
+   * @var {CanvasRenderingContext2D}  context   Canvas drawing context
+   * @var {Vec2.Vector}               location  Where to draw the blood
+   * 
+   * @return {void}
+   */
+  public spatter(context: CanvasRenderingContext2D, location: Vec2.Vector): void {
     let loop = 0;
 
     const x_min = Math.round(location.x-(Constants.ZOMBALL_BLOOD_SPLATTER_SIZE/2));
@@ -135,14 +291,21 @@ export default class World {
     }
   }
 
-  drawShotSplat(bloofVec: Vec2.Vector) {
+
+  /**
+   * Draws gunshot splatter
+   *
+   * @var {Vec2.Vector} 
+   * @var {[type]}
+   */
+  public drawShotSplat(bloodVec: Vec2.Vector) {
     if (this.background) {
       const bg_context: CanvasRenderingContext2D | null = this.background.getContext("2d");
 
       let loop = 0;
 
       while (loop < 5) {
-        const location = bloofVec.add(new Vec2.Vector(Random.int(-10, 10), Random.int(-10, 10)));
+        const location = bloodVec.add(new Vec2.Vector(Random.int(-10, 10), Random.int(-10, 10)));
         const size = Random.int(0, (Constants.ZOMBALL_BLOOD_SPLATTER_CIRCLE_MAX - Constants.ZOMBALL_BLOOD_SPLATTER_CIRCLE_MIN) + Constants.ZOMBALL_BLOOD_SPLATTER_CIRCLE_MIN);
         const one = Random.int(0, 55);
         const two = Random.int(0, 128);
@@ -154,14 +317,30 @@ export default class World {
     }
   }
 
-  drawSpatter(location: Vec2.Vector): void {
+
+  /**
+   * Draw splatter calling wrapper function
+   *
+   * @var {Vec2.Vector}   location  WHere splatter will be drawn
+   * 
+   * @return {void}
+   */
+  public drawSpatter(location: Vec2.Vector): void {
     if (this.background) {
       const bg_context: CanvasRenderingContext2D | null = this.background.getContext("2d");
       if (bg_context) this.spatter(bg_context, location);
     }
   }
 
-  drawDebug(context: CanvasRenderingContext2D): void {
+
+  /**
+   * Draw the debugging panel
+   *
+   * @param   {CanvasRenderingContext2D}  context  Canvas rendering context
+   *
+   * @return  {void}
+   */
+  public drawDebug(context: CanvasRenderingContext2D): void {
     // Draw number to the screen
     context.fillStyle = '#dddddd55';
     context.fillRect(Constants.GAME_SIZE.width - 75, Constants.GAME_SIZE.height - 100, 75, 100);
@@ -172,7 +351,14 @@ export default class World {
 
   }
 
-  drawGui(context: CanvasRenderingContext2D) {
+  /**
+   * Draw the HUD gui
+   *
+   * @param   {CanvasRenderingContext2D}  context  Canvas rendering context
+   *
+   * @return  {void}
+   */
+  public drawGui(context: CanvasRenderingContext2D): void {
     var health_bar_length = Constants.GAME_SIZE.width - 30;
     var health_bar_width = 30;
     var health_bar_empty_color = "#ff5555bb";
@@ -205,7 +391,16 @@ export default class World {
     context.fillText(`Score: ${this.player_score}`, 20, 35);
   }
 
-  render(canvas: HTMLCanvasElement, context: CanvasRenderingContext2D) {
+
+  /**
+   * Process all the rendering for the world
+   *
+   * @param   {HTMLCanvasElement}         canvas   Canvas
+   * @param   {CanvasRenderingContext2D}  context  Canvas drawing context
+   *
+   * @return  {void}
+   */
+  public render(canvas: HTMLCanvasElement, context: CanvasRenderingContext2D): void {
 
     // build background
     if (this.background == null) {
@@ -231,7 +426,15 @@ export default class World {
     this.drawGui(context);
   }
 
-  outOfRange(location: Vec2.Vector): boolean {
+
+  /**
+   * Check if a location vector is our of bounds
+   *
+   * @var {Vec2.Vector}   location  Location to check
+   * 
+   * @return {boolean}
+   */
+  public outOfRange(location: Vec2.Vector): boolean {
     let x = location.x;
     let y = location.y;
     if (x < (0 - Constants.ZOMBALL_SPAWN_OFFSET) || x > (Constants.GAME_SIZE.width + Constants.ZOMBALL_SPAWN_OFFSET)) {
@@ -243,7 +446,13 @@ export default class World {
     return false;
   }
 
-  removeEntities(): void {
+
+  /**
+   * Garbage clean up, removes any entities that are schedule for removal
+   *
+   * @return  {void} 
+   */
+  public removeEntities(): void {
     const ids: number[] = [];
     for (let [key, entity] of this.entities) {
       if (entity.remove) {
@@ -252,7 +461,16 @@ export default class World {
     }
   }
 
-  process(gameTime: number, timestamp: DOMHighResTimeStamp) : void {
+
+  /**
+   * Do all world calculations
+   *
+   * @param   {number}               gameTime   DeltaTime
+   * @param   {DOMHighResTimeStamp}  timestamp  Current time
+   *
+   * @return  {void}
+   */
+  public process(gameTime: number, timestamp: DOMHighResTimeStamp) : void {
     // Calculate the number of seconds passed since the last frame
     this.timeToDraw = gameTime / 1000;
 
@@ -266,7 +484,17 @@ export default class World {
     this.player?.process(gameTime);
   }
 
-  withinRange(vector1: Vec2.Vector, vector2: Vec2.Vector, range: number) : boolean {
+
+  /**
+   * Determine if vecor1 is witin range of vector2
+   *
+   * @var {Vec2.Vector} vector1   First location
+   * @var {Vec2.Vector} vector2   Second location
+   * @var {number}      range     Range in pixels
+   * 
+   * @return {boolean}
+   */
+  public withinRange(vector1: Vec2.Vector, vector2: Vec2.Vector, range: number) : boolean {
     const distance_vector: number = vector1.distance(vector2);
     if (distance_vector <= range) {
       return true;
@@ -274,7 +502,16 @@ export default class World {
     return false;
   }
 
-  getEntitiesInRange(queryEntity: Entity, range: number, type?: string): Entity[] {
+  /**
+   * Get entities within range, get any entities within range of given entity.
+   *
+   * @param   {Entity}    queryEntity  Source entity
+   * @param   {number}    range        Range from source entity
+   * @param   {string[]}  type         type filter out specific types of entities
+   *
+   * @return  {Entity[]}               List of entities
+   */
+  public getEntitiesInRange(queryEntity: Entity, range: number, type?: string): Entity[] {
     const entities: Entity[] = [];
     for (let [key, entity] of this.entities) {
       if (queryEntity.id != key) { 
@@ -292,7 +529,16 @@ export default class World {
     return entities;
   }
 
-  getCloseEntity(queryEntity: Entity, range: number, type?: string): Entity | null {
+  /**
+   * Get the closest entity to a given entity within a range
+   *
+   * @param   {Entity}  queryEntity  Source entity
+   * @param   {number}  range        range
+   * @param   {string}  type         type filter on type
+   *
+   * @return  {Entity}               Closest entity of type
+   */
+  public getCloseEntity(queryEntity: Entity, range: number, type?: string): Entity | null {
     for (let [key, entity] of this.entities) {
       // Exclude the queried entity
       if (queryEntity.id != key) { 
